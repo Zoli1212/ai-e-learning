602014d938a27e6add4c763e55b77a9541f202ec
zoltan-molnar <rdigital4455@gmail.com>
2025-11-23 14:23:24 +0100

    courses01

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -17,62 +17,62 @@ import { useParams, usePathname } from 'next/navigation'
 import { Course } from '../(routes)/courses/_components/CourseList'
 import { getCourses } from '@/actions/course'
 
-// const courses = [
-//     {
-//         id: 1,
-//         name: 'HTML',
-//         desc: 'Learn the fundamentals of HTML and build the structure of modern web pages.',
-//         path: '/course/1/detail'
-//     },
-//     {
-//         id: 2,
-//         name: 'CSS',
-//         desc: 'Master CSS to style and design responsive, visually appealing web layouts.',
-//         path: '/course/2/detail'
-//     },
-//     {
-//         id: 3,
-//         name: 'React',
-//         desc: 'Build dynamic and interactive web applications using the React JavaScript library.',
-//         path: '/course/3/detail'
-//     },
-//     {
-//         id: 4,
-//         name: 'React Advanced',
-//         desc: 'Deep dive into advanced React concepts including hooks, state management, performance optimization, and architectural patterns.',
-//         path: '/course/4/detail'
-//     },
-//     {
-//         id: 5,
-//         name: 'Python',
-//         desc: 'Learn Python programming from basics to intermediate level, covering logic building, functions, and real-world applications.',
-//         path: '/course/5/detail'
-//     },
-//     {
-//         id: 6,
-//         name: 'Python Advanced',
-//         desc: 'Master advanced Python concepts such as OOP, modules, APIs, data processing, and automation.',
-//         path: '/course/6/detail'
-//     },
-//     {
-//         id: 7,
-//         name: 'Generative AI',
-//         desc: 'Explore prompt engineering, LLMs, embeddings, image generation, and build GenAI-powered applications.',
-//         path: '/course/7/detail'
-//     },
-//     {
-//         id: 8,
-//         name: 'Machine Learning',
-//         desc: 'Understand ML concepts, algorithms, data preprocessing, model training, evaluation, and deployment.',
-//         path: '/course/8/detail'
-//     },
-//     {
-//         id: 9,
-//         name: 'JavaScript',
-//         desc: 'Learn core JavaScript concepts, asynchronous programming, DOM manipulation, and modern ES6+ features.',
-//         path: '/course/9/detail'
-//     }
-// ];
+const courses = [
+    {
+        id: 1,
+        name: 'HTML',
+        desc: 'Learn the fundamentals of HTML and build the structure of modern web pages.',
+        path: '/course/1/detail'
+    },
+    {
+        id: 2,
+        name: 'CSS',
+        desc: 'Master CSS to style and design responsive, visually appealing web layouts.',
+        path: '/course/2/detail'
+    },
+    {
+        id: 3,
+        name: 'React',
+        desc: 'Build dynamic and interactive web applications using the React JavaScript library.',
+        path: '/course/3/detail'
+    },
+    {
+        id: 4,
+        name: 'React Advanced',
+        desc: 'Deep dive into advanced React concepts including hooks, state management, performance optimization, and architectural patterns.',
+        path: '/course/4/detail'
+    },
+    {
+        id: 5,
+        name: 'Python',
+        desc: 'Learn Python programming from basics to intermediate level, covering logic building, functions, and real-world applications.',
+        path: '/course/5/detail'
+    },
+    {
+        id: 6,
+        name: 'Python Advanced',
+        desc: 'Master advanced Python concepts such as OOP, modules, APIs, data processing, and automation.',
+        path: '/course/6/detail'
+    },
+    {
+        id: 7,
+        name: 'Generative AI',
+        desc: 'Explore prompt engineering, LLMs, embeddings, image generation, and build GenAI-powered applications.',
+        path: '/course/7/detail'
+    },
+    {
+        id: 8,
+        name: 'Machine Learning',
+        desc: 'Understand ML concepts, algorithms, data preprocessing, model training, evaluation, and deployment.',
+        path: '/course/8/detail'
+    },
+    {
+        id: 9,
+        name: 'JavaScript',
+        desc: 'Learn core JavaScript concepts, asynchronous programming, DOM manipulation, and modern ES6+ features.',
+        path: '/course/9/detail'
+    }
+];
 
 
 function Header() {
@@ -88,7 +88,12 @@ function Header() {
     const GetCourses = async () => {
         const result = await getCourses();
         console.log(result);
-        setCourses(result);
+        if (result) {
+
+            setCourses(result);
+        } else {
+            setCourses(courses);
+        }
     }
 
     return (
